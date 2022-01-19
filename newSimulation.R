a16467c6dc2d5ea4cddfa07ce93ec10453f9f03d
Richard Lucas <rlucas11@yahoo.com>
2022-01-19 10:34:22 -0500

    Finished Simulation Loop

--- newSimulation.R
+++ newSimulation.R
@@ -10,45 +10,6 @@ source("scripts/ri_clpm10_c.R")
 source("scripts/starts_c.R")
 source("run_sim.R")
 
-## Testing
-studyN=25      # N to generate
-nwaves=10   # Number of waves
-ri_x=1     # Random intercept variance for X
-ri_y=1     # Random intercept variance for Y
-cor_i=.5   # Correlation between intercepts
-x=0        # AR variance for X
-y=0        # AR variance for Y
-stab_x=.5  # Stability of X
-stab_y=.5  # Stability of Y
-yx=0      # Cross lag (Y on X)
-xy=0      # Cross lag (X on Y)
-cor_xy=.5  # Correlation between X and Y
-cor_xyr=.25 # Correlation between X and Y residuals
-xr=.25       # Measurement error for X
-yr=.25       # Measurement error for Y
-reliability_x <- .80
-reliability_y <- .80
-
-dataS <- gen_starts(n=studyN,
-                    nwaves = nwaves,
-                    ri_x = ri_x,
-                    ri_y = ri_y,
-                    cor_i = cor_i,
-                    x = x,
-                    y = y,
-                    stab_x = stab_x,
-                    stab_y = stab_y,
-                    yx = yx,
-                    xy = xy,
-                    cor_xy = cor_xy,
-                    xr = xr,
-                    yr = yr
-                    )
-temp <- lavaan(clpm10_c, dataS)
-parameterEstimates(temp)
-
-
-
 ## Function to simulate clpm data
 run_sim_clpm <- function(studyN,      
                        ri_x,     
@@ -90,6 +51,42 @@ run_sim_clpm <- function(studyN,
 }
 
 
+## Testing
+studyN=25      # N to generate
+nwaves=10   # Number of waves
+ri_x=1     # Random intercept variance for X
+ri_y=1     # Random intercept variance for Y
+cor_i=.5   # Correlation between intercepts
+x=0        # AR variance for X
+y=0        # AR variance for Y
+stab_x=.5  # Stability of X
+stab_y=.5  # Stability of Y
+yx=0      # Cross lag (Y on X)
+xy=0      # Cross lag (X on Y)
+cor_xy=.5  # Correlation between X and Y
+cor_xyr=.25 # Correlation between X and Y residuals
+xr=.25       # Measurement error for X
+yr=.25       # Measurement error for Y
+reliability_x <- .80
+reliability_y <- .80
+
+dataS <- gen_starts(n=studyN,
+                    nwaves = nwaves,
+                    ri_x = ri_x,
+                    ri_y = ri_y,
+                    cor_i = cor_i,
+                    x = x,
+                    y = y,
+                    stab_x = stab_x,
+                    stab_y = stab_y,
+                    yx = yx,
+                    xy = xy,
+                    cor_xy = cor_xy,
+                    xr = xr,
+                    yr = yr
+                    )
+temp <- lavaan(clpm10_c, dataS)
+parameterEstimates(temp)
 
 sims <- data.frame(t(replicate(n=1000, run_sim_clpm(studyN=25,      # N to generate
                                                    ri_x=1,     # Random intercept variance for X
@@ -108,9 +105,28 @@ sims <- data.frame(t(replicate(n=1000, run_sim_clpm(studyN=25,      # N to gener
                                simplify = TRUE)))
 
 
+run_sim_clpm(50, 1, 1, .5, 1, 1, .5, .5, 0, 0, .5, .8, .8)
 
-## Initialize Matrix
 
+
+
+
+################################################################################
+## Simulation
+################################################################################
+
+## Test Simulation Values
+
+nValues <- c(100,250)
+rValues <- c(.3, .5)
+reliabilities <- c(.5, .7)
+## sValues1 <- 1/reliabilities-1
+## sValues2 <- 1.5/reliabilities-1.5
+## sValues3 <- 2/reliabilities-2
+## sValues4 <- 3/reliabilities-3
+arValues <- c(0,1)
+
+## Actual Simulation Values
 nValues <- c(25,50,100,250,500,1000)
 rValues <- c(.1,.3,.5,.7)
 reliabilities <- c(.5, .7, .9)
@@ -121,48 +137,53 @@ sValues4 <- 3/reliabilities-3
 arValues <- c(0, .5, 1, 2)
 
 
+
 ## Run Sim
 ##  
 loopRow <- 1
-results <- data.frame(value1 = numeric(),
-                      value2 = numeric(),
-                      value3 = numeric(),
-                      value4 = numeric(),
-                      value5 = numeric(),
+results <- data.frame(N = numeric(),
+                      r = numeric(),
+                      reliability = numeric(),
+                      AR_Var = numeric(),
                       power = numeric(),
                       estimatex = numeric(),
                       estimatey = numeric())
 for (i in 1:length(nValues)) {
     for (j in 1:length(rValues)) {
         for (k in 1:length(reliabilities)) {
-            for (l in 1:length(arValues) {
-                nValue <- values1[i]
-                rValue <- values2[j]
+            for (l in 1:length(arValues)) {
+                nValue <- nValues[i]
+                rValue <- rValues[j]
                 rel <- reliabilities[k]
                 arValue <- arValues[l]
                 sims <- data.frame(t(mcreplicate(n=1000, run_sim_clpm(studyN=nValue,      # N to generate
-                                                                      nwaves=10,   # Number of waves
                                                                       ri_x=1,     # Random intercept variance for X
                                                                       ri_y=1,     # Random intercept variance for Y
                                                                       cor_i=rValue,   # Correlation between intercepts
-                                                                      x=1,        # AR variance for X
-                                                                      y=1,        # AR variance for Y
+                                                                      x=arValue,        # AR variance for X
+                                                                      y=arValue,        # AR variance for Y
                                                                       stab_x=.5,  # Stability of X
                                                                       stab_y=.5,  # Stability of Y
                                                                       yx=0,      # Cross lag (Y on X)
                                                                       xy=0,      # Cross lag (X on Y)
                                                                       cor_xy=.5,  # Correlation between X and Y
-                                                                      xr=.5,       # Measurement error for X
-                                                                      yr=.5       # Measurement error for Y
+                                                                      reliability_x=rel,       # Measurement error for X
+                                                                      reliability_y=rel       # Measurement error for Y
                                                                       ), mc.cores=14)))
+                results[loopRow,1] <- nValue
+                results[loopRow,2] <- rValue
+                results[loopRow,3] <- rel
+                results[loopRow,4] <- arValue
+                results[loopRow,5] <- sum(sims$X2<.05 | sims$X4<.05)/1000
+                results[loopRow,6] <- mean(sims$X1)
+                results[loopRow,7] <- mean(sims$X3)
                 loopRow <- loopRow+1
-                results[loopRow,1] <- value1
-                results[loopRow,2] <- value2
-                results[loopRow,3] <- sum(sims$X2<.05 | sims$X4<.05)/1000
-                results[loopRow,4] <- mean(sims$X1)
-                results[loopRow,5] <- mean(sims$X3)
             }
-            }
+        }
+    }
+}
+
+                
 
 
 
@@ -171,3 +192,22 @@ library(ggplot2)
 ggplot(aes(x=value1, y=power, group=value2), data=results) + geom_line()
 
 + scale_x_log10()
+
+
+
+
+### Scratch
+    mcreplicate(n=1000, run_sim_clpm(studyN=nValue,      # N to generate
+                                     ri_x=1,     # Random intercept variance for X
+                                     ri_y=1,     # Random intercept variance for Y
+                                     cor_i=rValue,   # Correlation between intercepts
+                                     x=arValue,        # AR variance for X
+                                     y=arValue,        # AR variance for Y
+                                     stab_x=.5,  # Stability of X
+                                     stab_y=.5,  # Stability of Y
+                                     yx=0,      # Cross lag (Y on X)
+                                     xy=0,      # Cross lag (X on Y)
+                                     cor_xy=.5,  # Correlation between X and Y
+                                     reliability_x=rel,       # Measurement error for X
+                                     reliability_y=rel       # Measurement error for Y
+                                     ), mc.cores=14)            
