11d4347790713f764d721cdf2e11e950d960aa7a
Rich Lucas <rlucas11@yahoo.com>
2022-03-31 15:51:25 -0400

    corrected minor summarizeR error

--- scripts/usefulFunctions.R
+++ scripts/usefulFunctions.R
@@ -7,7 +7,7 @@ summarizeR <- function(corMat, nvars) {
         for (i in 1:((nrow(corMat)/nvars)-1)) {
             sumR <- 0
             for (j in seq(1, (nrow(corMat)-nvars*i), by=nvars)) {
-                sumR <- sumR + corMat[(i+k+j+(i-1)), j+(k-1)]
+                sumR <- sumR + corMat[(i+(k-1)+j), j+(k-1)]
             }
             averageRs[i,k] <- sumR/((nrow(corMat)/nvars)-i)
         }
@@ -19,6 +19,9 @@ corMat <- matrix(c(1, .5, .25,
                    .5, 1, .25,
                    .25, .5, 1),
                  nrow=3,
-                 ncol=3)
+                 ncol=3,
+                 byrow=TRUE)
 
 summarizeR(corMat, nvars = 1)
+
+
